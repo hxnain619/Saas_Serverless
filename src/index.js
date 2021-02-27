da448e727b365908bf32061ec95a633039ecf0b6
hxnain619 <hxan619@gmail.com>
2021-02-27 17:31:49 +0500

    some changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
   render() {
-    return <h1>Hello World from React boilerplate</h1>;
+    return <h1>Hello World, I am training Sass</h1>;
   }
 }
 ReactDOM.render(<App />, document.getElementById('root'));
