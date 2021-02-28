8a4ad77c1845be48a3c42f1c3ac52b063efd8dbf
hxnain619 <hxan619@gmail.com>
2021-02-28 19:17:26 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
   render() {
-    return <h1>Hello World, I am Learning Sass Technologies. Deploying again...</h1>;
+    return <h1>Hi, this is Hasnain and i am learning Sass.</h1>;
   }
 }
 ReactDOM.render(<App />, document.getElementById('root'));
