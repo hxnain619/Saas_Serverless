8cca999e9ec9c6fb8e326c86d7f22589f7ad8f0b
hxnain619 <hxan619@gmail.com>
2021-02-28 20:08:03 +0500

    removed Build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
   render() {
-    return <h1>Hi, this is Hasnain and i am learning Sass.</h1>;
+    return <h1>Hi, this is Hasnain and i am learning Sass.Test Deployment..</h1>;
   }
 }
 ReactDOM.render(<App />, document.getElementById('root'));
